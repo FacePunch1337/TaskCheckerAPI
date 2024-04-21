6cb24ab9b5097c209d020df3ec9da889baa33ddf
unknown <rezol1337@gmail.com>
2024-04-21 16:50:26 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const User = require('./models/userModel.js');
+const bcrypt = require('bcrypt'); // Добавляем bcrypt
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Маршруты
+
+// Получить всех пользователей
+app.get('/users', async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+// Получить пользователя по ID
+app.get('/users/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const user = await User.findById(id);
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+// Создать нового пользователя
+app.post('/users', async (req, res) => {
+    try {
+        // Хэширование пароля перед сохранением в базу данных
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        const user = await User.create({ username: req.body.username, password: hashedPassword });
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+// Обновить пользователя по ID
+app.put('/users/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const user = await User.findByIdAndUpdate(id, req.body);
+        if (!user) {
+            return res.status(404).json({ message: `cannot find any user with ID ${id}` });
+        }
+        const updatedUser = await User.findById(id);
+        res.status(200).json(updatedUser);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+// Удалить пользователя по ID
+app.delete('/users/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const user = await User.findByIdAndDelete(id);
+        if (!user) {
+            return res.status(404).json({ message: `cannot find any user with ID ${id}` });
+        }
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+mongoose.set("strictQuery", false);
+mongoose.connect('mongodb+srv://rezol1337:GVDGGnZDTVrT6zRi@cluster0.w3rkzvn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+    .then(() => {
+        console.log('Connected to MongoDB');
+        app.listen(3000, () => {
+            console.log(`Node API app is running on port 3000`);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
