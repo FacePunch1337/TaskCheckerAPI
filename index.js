1101a91a4e4cb55d8c2c3da3b0e3a33023ec72a5
unknown <rezol1337@gmail.com>
2024-04-26 22:51:10 +0300

    upload

--- index.js
+++ index.js
@@ -10,18 +10,18 @@ const port = process.env.PORT || 8000;
 
 app.use(express.json());
 app.use("/api", api);
-app.use('/uploads', express.static('uploads'));
 app.use(express.urlencoded({ extended: false }));
 
 // Настройка multer для обработки файлов
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/');
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  }
-});
+    destination: function (req, file, cb) {
+      const userId = req.params.userId; // Получаем userId из параметра запроса
+      cb(null, `uploads/users/${userId}/avatar`); // Указываем путь для сохранения файла
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    }
+  });
 
 const upload = multer({ storage: storage });
 
