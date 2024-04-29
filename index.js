0702897c094360c6caaedb4967ae0aa285f29169
unknown <rezol1337@gmail.com>
2024-04-29 19:12:24 +0300

    upload

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const upload = multer();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
+app.use(express.static(path.join(__dirname, 'public')));
 // Получить всех пользователей
 app.get('/users', async (req, res) => {
   try {
