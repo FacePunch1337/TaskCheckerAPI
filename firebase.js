660d342c76930e7252b3a12af8339458023f7368
unknown <rezol1337@gmail.com>
2024-04-29 18:43:09 +0300

    upload

--- firebase.js
+++ firebase.js
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCnlcR-1pnqtuGD2WcAuTIk0EKbvFLpzro",
   authDomain: "taskcheker-39fd8.firebaseapp.com",
@@ -19,3 +15,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const storage = getStorage(app);
