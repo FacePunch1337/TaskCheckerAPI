4f420794d0804ac99cd2828b82c0badb781b9016
unknown <rezol1337@gmail.com>
2024-05-08 20:37:33 +0300

    workSpace-createBoard

--- models/boardModel.js
+++ models/boardModel.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose');
 const boardSchema = mongoose.Schema(
     {
         title: {
-            type: String,
-            unique: true
+            type: String
         }
         
         
